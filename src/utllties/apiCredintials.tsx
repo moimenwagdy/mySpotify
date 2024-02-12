ecb62bf8f88cd3b2b08b5aa570c302bb69cd7ece
moimenwagdy <moimenwy@gmail.com>
2024-02-12 19:50:54 +0200

    predeployTest

--- src/utllties/apiCredintials.tsx
+++ src/utllties/apiCredintials.tsx
@@ -1,5 +1,7 @@
 export const clientID: string = "4775036253ed458293ae5f469b97b5c9";
-export const redirectURI: string = "http://localhost:5173/home";
+// export const redirectURI: string = "http://localhost:5173/home";
+export const redirectURI: string =
+  "https://curious-crisp-b3cf39.netlify.app//home";
 export const endPoint: string = "https://accounts.spotify.com/authorize";
 export const responseType: string = "token";
 export const client_secret: string = "528071f575a1422e963a93a574d41ee1";

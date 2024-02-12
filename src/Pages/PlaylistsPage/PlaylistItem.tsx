1b562d23d58078b55e95925f8e5c10f7ce2e593b
moimenwagdy <moimenwy@gmail.com>
2024-02-12 19:36:19 +0200

    playlist pagenation set Offset

--- src/Pages/PlaylistsPage/PlaylistItem.tsx
+++ src/Pages/PlaylistsPage/PlaylistItem.tsx
@@ -1,95 +1,114 @@
-import { Link, json } from "react-router-dom";
-import { LoaderFunction, LoaderFunctionArgs, defer } from "react-router";
+import { Link, json, useSearchParams } from "react-router-dom";
+import {
+  LoaderFunction,
+  LoaderFunctionArgs,
+  defer,
+  useParams,
+} from "react-router";
 import { myToken } from "../../utllties/setFutureDate";
 import { playlistItem } from "./Types";
 import Button from "../../uiux/Button";
 import { AnimatePresence, motion } from "framer-motion";
 import { useAppSelector, useAppDispatch } from "../../stateRoot/reduxHooks";
 import { exitAction } from "../../stateRoot/exitSlice";
+
 const PlaylistItem: React.FC = () => {
-  // const [params] = useSearchParams();
-  // const offset = params.get("offset");
   const dispatch = useAppDispatch();
+  const param = useParams();
+  const id = param.id;
   const data = useAppSelector((state) => state.playlistResponseSlice);
   const exit = useAppSelector((state) => state.exitSlice.exiting);
   function playlistOpen(): void {
     dispatch(exitAction.setExit());
   }
+  const [searchParams] = useSearchParams();
+  const offset = searchParams.get("offset");
   console.log(data);
+
   return (
-    <AnimatePresence>
-      {!exit && (
-        <motion.main
-          variants={{
-            hidden: { opacity: 0, y: -30 },
-            visible: { opacity: 1, y: 0 },
-          }}
-          initial="hidden"
-          animate="visible"
-          exit={{
-            opacity: 0,
-            transition: { type: "spring" },
-          }}>
-          <section className=" text-center mt-2">
-            <h1 className="text-center text-lg font-[600]">{data.message}</h1>
-          </section>
-          <motion.ul
+    <>
+      <div className="flex justify-center gap-x-10">
+        <Link to={`/playlists/${id}/?limit=10&offset=${Number(offset) - 10}`}>
+          <button disabled={data.playlists.previous === null}>previous</button>
+        </Link>
+        <Link to={`/playlists/${id}/?limit=10&offset=${Number(offset) + 10}`}>
+          <button disabled={data.playlists.next === null}>next</button>
+        </Link>
+      </div>
+      <AnimatePresence>
+        {!exit && (
+          <motion.main
             variants={{
-              visible: { transition: { staggerChildren: 0.05 } },
+              hidden: { opacity: 0, y: -30 },
+              visible: { opacity: 1, y: 0 },
             }}
-            className={`mt-2 mx-auto w-[97%] rounded-md bg-darkGreen  ${
-              data.playlists.items.length < 6
-                ? "min-h-[200px]"
-                : "min-h-[560px]"
-            }   flex gap-3 py-3 px-2 flex-wrap justify-center`}>
-            {data &&
-              data.playlists.items.map((item: playlistItem) => {
-                return (
-                  <motion.li
-                    key={item.name}
-                    className="w-full cursor-pointer md:max-h-44 sm:w-4/5 group hover:bg-dark/90 md:w-4/5 lg:min-w-[33%] rounded-md lg:max-w-[49%] bg-dark/80  flex p-1 "
-                    variants={{
-                      hidden: { opacity: 0, y: -50 },
-                      visible: { opacity: 1, y: 0 },
-                    }}
-                    whileHover={{ scale: 1.01 }}>
-                    <a
-                      onClick={playlistOpen}
-                      href="/categories"
-                      className="flex w-full h-full gap-x-1">
-                      <img
-                        className="w-28 rounded-md grow-0"
-                        src={item.images[0].url}
-                        alt="Pl"></img>
-                      <div className="flex flex-col justify-between items-start w-full">
-                        <article className="text-wrap">
-                          <h2 className="text-lg font-[700] text-lightGreen">
-                            {item.name}
-                          </h2>
-                          <p className="text-sm text-white/70 max-w-full 	text-pretty">
-                            {item.description}
+            initial="hidden"
+            animate="visible"
+            exit={{
+              opacity: 0,
+              transition: { type: "spring" },
+            }}>
+            <section className=" text-center mt-2">
+              <h1 className="text-center text-lg font-[600]">{data.message}</h1>
+            </section>
+            <motion.ul
+              variants={{
+                visible: { transition: { staggerChildren: 0.05 } },
+              }}
+              className={`mt-2 mx-auto w-[97%] rounded-md bg-darkGreen  ${
+                data.playlists.items.length < 6
+                  ? "min-h-[200px]"
+                  : "min-h-[560px]"
+              }   flex gap-3 py-3 px-2 flex-wrap justify-center`}>
+              {data &&
+                data.playlists.items.map((item: playlistItem) => {
+                  return (
+                    <motion.li
+                      key={item.name}
+                      className="w-full cursor-pointer md:max-h-44 sm:w-4/5 group hover:bg-dark/90 md:w-4/5 lg:min-w-[33%] rounded-md lg:max-w-[49%] bg-dark/80  flex p-1 "
+                      variants={{
+                        hidden: { opacity: 0, y: -50 },
+                        visible: { opacity: 1, y: 0 },
+                      }}
+                      whileHover={{ scale: 1.01 }}>
+                      <a
+                        onClick={playlistOpen}
+                        href="/categories"
+                        className="flex w-full h-full gap-x-1">
+                        <img
+                          className="w-28 rounded-md grow-0"
+                          src={item.images[0].url}
+                          alt="Pl"></img>
+                        <div className="flex flex-col justify-between items-start w-full">
+                          <article className="text-wrap">
+                            <h2 className="text-lg font-[700] text-lightGreen">
+                              {item.name}
+                            </h2>
+                            <p className="text-sm text-white/70 max-w-full 	text-pretty">
+                              {item.description}
+                            </p>
+                          </article>
+                          <div className="ms-1">
+                            <Link to="tracks">
+                              <Button
+                                title="Tracks"
+                                className="bg-dark group-hover:bg-light group-hover:text-dark duration-300 delay-200 px-3 py-2 text-darkGreen font-[600]"
+                              />
+                            </Link>
+                          </div>
+                          <p className="text-xs self-end mt-[-10px] text-white/50 me-1">
+                            Owner {item.owner.display_name}
                           </p>
-                        </article>
-                        <div className="ms-1">
-                          <Link to="tracks">
-                            <Button
-                              title="Tracks"
-                              className="bg-dark group-hover:bg-light group-hover:text-dark duration-300 delay-200 px-3 py-2 text-darkGreen font-[600]"
-                            />
-                          </Link>
                         </div>
-                        <p className="text-xs self-end mt-[-10px] text-white/50 me-1">
-                          Owner {item.owner.display_name}
-                        </p>
-                      </div>
-                    </a>
-                  </motion.li>
-                );
-              })}
-          </motion.ul>
-        </motion.main>
-      )}
-    </AnimatePresence>
+                      </a>
+                    </motion.li>
+                  );
+                })}
+            </motion.ul>
+          </motion.main>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
--- src/Pages/PlaylistsPage/PlaylistItems.tsx
+++ src/Pages/PlaylistsPage/PlaylistItems.tsx
@@ -1,46 +1,34 @@
 import PlaylistItem from "./PlaylistItem";
-import { useAppDispatch, useAppSelector } from "../../stateRoot/reduxHooks";
+import { useAppDispatch } from "../../stateRoot/reduxHooks";
 import { exitAction } from "../../stateRoot/exitSlice";
-import { Await, useLoaderData, useParams } from "react-router";
+import { Await, useLoaderData } from "react-router";
 import playlistType from "./Types";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import playlistsResponseSlice from "../../stateRoot/playlistsResponseSlice";
 import LoadingIndecator from "../../components/LoadingIndecator";
-import playlistPages from "../../stateRoot/playlistPages";
-import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 exitAction;
 const PlaylistItems = () => {
-  // const data = useAppSelector((state) => state.playlistResponseSlice);
-
-  const pagesData = useAppSelector((state) => state.playlistPages);
-  const param = useParams();
-  const id = param.id;
-  const dispatch = useAppDispatch();
-
   const { playlists } = useLoaderData() as playlistType;
+  Promise.resolve(playlists).then((playlists) => {
+    dispatch(playlistsResponseSlice.actions.setPlaylistData({ playlists }));
+  });
+  const [myOffset, setMyOffset] = useState("");
+  const dispatch = useAppDispatch();
 
   function backToCateg(): void {
     dispatch(exitAction.setExit());
   }
-
-  Promise.resolve(playlists).then((playlists) => {
-    dispatch(playlistsResponseSlice.actions.setPlaylistData({ playlists }));
-  });
-
-  function nextPlayLists() {
-    dispatch(playlistPages.actions.nextPage());
-  }
-  function prvPlaylist() {
-    dispatch(playlistPages.actions.prevPage());
-  }
-
+  const [searchPrams] = useSearchParams();
+  const offset = searchPrams.get("offset") as string;
+  useEffect(() => setMyOffset(offset), [offset]);
   return (
     <>
       <Suspense fallback={<LoadingIndecator />}>
         <Await resolve={playlists}>
           {() => {
-            return <PlaylistItem />;
+            return <PlaylistItem key={myOffset} />;
           }}
         </Await>
       </Suspense>
@@ -50,18 +38,6 @@ const PlaylistItems = () => {
         href="/categories">
         Back To Categories
       </a>
-      <div className="flex justify-center gap-x-10">
-        <Link
-          to={`/playlists/${id}/?limit=10&offset=${10 || pagesData.offset}`}
-          onClick={prvPlaylist}>
-          previous
-        </Link>
-        <Link
-          to={`/playlists/${id}/?limit=10&offset=${pagesData.offset}`}
-          onClick={nextPlayLists}>
-          next
-        </Link>
-      </div>
     </>
   );
 };

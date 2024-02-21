5d812124643b0aa9200c8581a821b631e75bb3b1
moimenwagdy <moimenwy@gmail.com>
2024-02-21 18:46:58 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://main--myspotfy.netlify.app/"
+  base: "https://main--myspotfy.netlify.app/",
 });

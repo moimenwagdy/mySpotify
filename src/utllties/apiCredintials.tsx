cffbb113b0c393684e21a27a7ad6a75bdce09a06
moimenwagdy <moimenwy@gmail.com>
2024-02-29 21:34:05 +0200

    test

--- src/utllties/apiCredintials.tsx
+++ src/utllties/apiCredintials.tsx
@@ -1,6 +1,6 @@
 export const clientID: string = "4775036253ed458293ae5f469b97b5c9";
-export const redirectURI: string = "http://localhost:5173/home";
-// export const redirectURI: string = "https://main--myspotfy.netlify.app/home";
+// export const redirectURI: string = "http://localhost:5173/home";
+export const redirectURI: string = "https://main--myspotfy.netlify.app/home";
 export const endPoint: string = "https://accounts.spotify.com/authorize";
 export const responseType: string = "token";
 export const client_secret: string = "528071f575a1422e963a93a574d41ee1";
